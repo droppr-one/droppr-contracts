e16bbabcf03c52011a8d7ec6238fd00d9bc3bd15
t0mcr8se <husamirami@gmail.com>
2022-03-14 00:24:05 +0300

    add deployment script and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
@@ -8,35 +8,41 @@ import "hardhat-gas-reporter";
 import "solidity-coverage";
 
 dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "0xabc123abc123abc123abc123abc123abc123abc123abc123abc123abc123abc1";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    andromeda: {
+      url: "https://andromeda.metis.io/?owner=1088",
+      accounts: [PRIVATE_KEY],
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    stardust: {
+      url: "https://stardust.metis.io/?owner=588",
+      accounts: [PRIVATE_KEY],
+    },
+    fuse: {
+      url: "https://rpc.fuse.io",
+      accounts: [PRIVATE_KEY],
+    },
+    spark: {
+      url: "https://rpc.fusespark.io",
+      accounts: [PRIVATE_KEY],
+    },
   },
 };
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,23 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+const SERVICE_FEE = ethers.utils.parseEther("0.05").toString();
+const PARTNER_FEE = ethers.utils.parseEther("0.5").toString();
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await ethers.getSigners();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  console.log("Deploying contracts with the account:", deployer.address);
+  const AirdropDistributor = await ethers.getContractFactory(
+    "AirdropDistributor"
+  );
 
-  await greeter.deployed();
+  const droppr = await AirdropDistributor.deploy(SERVICE_FEE, PARTNER_FEE);
+  console.log("txHash:", droppr.deployTransaction.hash);
+  await droppr.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("droppr just dropped at: ", droppr.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
